1607ff2629ae1782aa070fe9cd99af56df28136c
tulenenok <small-chinchilla@yandex.ru>
2022-07-13 14:36:30 +0300

    edit

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.sirius.test_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.RatingBar
+import androidx.appcompat.app.AppCompatActivity
+import android.widget. TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        var smallRatingBar: RatingBar = findViewById(R.id.ratingBar)
+//        smallRatingBar.max = 5
+//        smallRatingBar.numStars = 5
+//        smallRatingBar.setRating(5.toFloat())
+       // findViewById(R.id.ratingBar).setIsIndicator(true)
     }
 }
